dd6f34c20bfdabce5edbf0c48166cb60761aa16b
Louie-Shen <71159008+Louie-Shen@users.noreply.github.com>
2024-05-26 12:11:45 -0400

    React app created
    React app created + basics being created

---
+++ my-app/src/Textbox.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Textbox() {
+  return (
+    <textarea id="textbox">
+      Type here...
+    </textarea>
+
+  );
+}
+
+export default Textbox;
---
+++ my-app/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Textbox from './Textbox';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <h1>Subway Surfer Video Generator</h1>
+    <Textbox />
+  </React.StrictMode>
+);
