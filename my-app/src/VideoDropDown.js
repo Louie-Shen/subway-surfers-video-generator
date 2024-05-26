69c6ef1f815b040687310ba317f4453fbeae4878
Louie-Shen <71159008+Louie-Shen@users.noreply.github.com>
2024-05-26 13:42:53 -0400

    added dropdown for video

---
+++ my-app/src/VideoDropDown.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function VideoDropDown() {
+    return(
+    <select name="select" id="videos">
+    <option value="subway">Subway Surfers</option>
+    <option value="minecraft">Minecraft Parkour</option>
+    </select>
+    );
+}
+
+export default VideoDropDown
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Textbox from './Textbox';
 import Generate from './Generate';
+import VideoDropDown from './VideoDropDown';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <h1>Subway Surfer Video Generator</h1>
     <Textbox />
-    <Generate />
+    <Generate /> <VideoDropDown />
   </React.StrictMode>
 );
