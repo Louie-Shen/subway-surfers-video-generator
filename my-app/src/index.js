efeaf7c94ab48cdc34962cd9d6c0cc93c8c865fd
Louie-Shen <71159008+Louie-Shen@users.noreply.github.com>
2024-05-31 20:40:26 -0400

    Voice Dropdown + controls container created

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom/client';
 import Textbox from './Textbox';
 import Generate from './Generate';
 import VideoDropDown from './VideoDropDown';
+import VoiceDropDown from './VoiceDropDown'
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <h1>Subway Surfer Video Generator</h1>
     <Textbox />
-    <Generate /> <VideoDropDown />
+    <div className="controls-container">
+    <VideoDropDown /> <VoiceDropDown /> <Generate /> 
+    </div>
   </React.StrictMode>
+  
 );
